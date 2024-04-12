19b639cd2cc06670b0caa626d0e13ab0bad9efa3
Jonny Burger <jonathanburger11@gmail.com>
2024-04-12 10:25:09 +0200

    monospace font

--- config/fonts.ts
+++ config/fonts.ts
@@ -3,23 +3,16 @@ import {
   loadFont as loadRegular,
 } from "@remotion/google-fonts/Inter";
 
-import {
-  fontFamily as monospaceFont,
-  loadFont as loadMonospace,
-} from "@remotion/google-fonts/RobotoCondensed";
-
 const regular = loadRegular();
-const monospace = loadMonospace();
 
 export const waitForFonts = async () => {
   await regular.waitUntilDone();
-  await monospace.waitUntilDone();
 };
 
 export const REGULAR_FONT_FAMILY = regularFont;
 export const REGULAR_FONT_WEIGHT = 600;
 
-export const MONOSPACE_FONT_FAMILY = monospaceFont;
+export const MONOSPACE_FONT_FAMILY = "GT Planar";
 export const MONOSPACE_FONT_WEIGHT = 500;
 
 export const TITLE_FONT_FAMILY = regularFont;
