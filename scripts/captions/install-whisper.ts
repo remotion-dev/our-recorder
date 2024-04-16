efc52a42be5017559e41bd5ba674b654a2b67dc3
Jonny Burger <jonathanburger11@gmail.com>
2024-04-16 07:50:04 +0200

    update progress

--- scripts/captions/install-whisper.ts
+++ scripts/captions/install-whisper.ts
@@ -2,12 +2,12 @@ import {
   downloadWhisperModel,
   installWhisperCpp,
 } from "@remotion/install-whisper-cpp";
-import { WHISPER_MODEL, WHISPER_PATH } from "../../config/whisper";
+import { WHISPER_MODEL, WHISPER_PATH, WHISPER_REF } from "../../config/whisper";
 
 export const ensureWhisper = async () => {
   await installWhisperCpp({
     to: WHISPER_PATH,
-    version: "9fab28135c7867bb7eccd9ebcd2ea8d52e42ca81",
+    version: WHISPER_REF,
     printOutput: true,
   });
   await downloadWhisperModel({
