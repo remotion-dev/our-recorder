f304457fa350575b9a068ae464645130b2184f90
Jonny Burger <jonathanburger11@gmail.com>
2024-05-17 15:04:00 -0700

    coool

---
+++ remotion/calculate-metadata/add-durations-to-scenes.ts
@@ -0,0 +1,115 @@
+import { CanvasLayout } from "../../config/layout";
+import { SceneAndMetadata } from "../../config/scenes";
+import { SCENE_TRANSITION_DURATION } from "../../config/transitions";
+import {
+  getShouldTransitionIn,
+  getShouldTransitionOut,
+} from "../animations/transitions";
+import { applyBRollRules } from "../scenes/BRoll/apply-b-roll-rules";
+
+const PLACEHOLDER_DURATION_IN_FRAMES = 60;
+
+export const addDurationsToScenes = (
+  scenes: SceneAndMetadata[],
+  canvasLayout: CanvasLayout,
+): {
+  durationInFrames: number;
+  scenesAndMetadataWithDuration: SceneAndMetadata[];
+} => {
+  let addedUpDurations = 0;
+  let currentChapter: string | null = null;
+
+  const scenesAndMetadataWithDuration: SceneAndMetadata[] = scenes.map(
+    (sceneAndMetadata, i) => {
+      const previousSceneAndMetaData = scenes[i - 1] ?? null;
+      const nextSceneAndMetaData = scenes[i + 1] ?? null;
+
+      const isTransitioningIn = previousSceneAndMetaData
+        ? getShouldTransitionIn({
+            previousScene: previousSceneAndMetaData,
+            scene: sceneAndMetadata,
+            canvasLayout: canvasLayout,
+          })
+        : false;
+
+      const isTransitioningOut = getShouldTransitionOut({
+        sceneAndMetadata,
+        nextScene: nextSceneAndMetaData,
+        canvasLayout: canvasLayout,
+      });
+
+      if (isTransitioningIn) {
+        addedUpDurations -= SCENE_TRANSITION_DURATION;
+      }
+
+      const from = addedUpDurations;
+      addedUpDurations += sceneAndMetadata.durationInFrames;
+
+      if (sceneAndMetadata.type === "other-scene") {
+        return {
+          ...sceneAndMetadata,
+          from,
+        };
+      }
+
+      let adjustedDuration = sceneAndMetadata.durationInFrames;
+
+      let transitionAdjustedStartFrame = sceneAndMetadata.startFrame;
+
+      if (isTransitioningIn) {
+        transitionAdjustedStartFrame = Math.max(
+          0,
+          sceneAndMetadata.startFrame - SCENE_TRANSITION_DURATION,
+        );
+
+        const additionalTransitionFrames =
+          sceneAndMetadata.startFrame - transitionAdjustedStartFrame;
+
+        addedUpDurations += additionalTransitionFrames;
+        adjustedDuration += additionalTransitionFrames;
+      }
+
+      if (sceneAndMetadata.scene.newChapter) {
+        currentChapter = sceneAndMetadata.scene.newChapter;
+      }
+
+      const retValue: SceneAndMetadata = {
+        ...sceneAndMetadata,
+        bRolls: applyBRollRules({
+          bRolls: sceneAndMetadata.bRolls,
+          sceneDurationInFrames: adjustedDuration,
+          willTransitionToNextScene: isTransitioningOut,
+        }),
+        startFrame: transitionAdjustedStartFrame,
+        durationInFrames: adjustedDuration,
+        from,
+        chapter: currentChapter,
+      };
+
+      if (sceneAndMetadata.scene.stopChapteringAfterThis) {
+        currentChapter = null;
+      }
+
+      return retValue;
+    },
+  );
+
+  if (scenesAndMetadataWithDuration.length === 0) {
+    scenesAndMetadataWithDuration.push({
+      type: "other-scene" as const,
+      scene: {
+        type: "noscenes" as const,
+        music: "none",
+        transitionToNextScene: true,
+      },
+      durationInFrames: PLACEHOLDER_DURATION_IN_FRAMES,
+      from: 0,
+    });
+    addedUpDurations += PLACEHOLDER_DURATION_IN_FRAMES;
+  }
+
+  return {
+    durationInFrames: addedUpDurations,
+    scenesAndMetadataWithDuration: scenesAndMetadataWithDuration,
+  };
+};
--- remotion/calculate-metadata/calc-metadata.ts
+++ remotion/calculate-metadata/calc-metadata.ts
@@ -1,18 +1,10 @@
 import type { CalculateMetadataFunction } from "remotion";
 import { FPS } from "../../config/fps";
-import { type SceneAndMetadata } from "../../config/scenes";
-import { SCENE_TRANSITION_DURATION } from "../../config/transitions";
 import type { MainProps } from "../Main";
-import {
-  getShouldTransitionIn,
-  getShouldTransitionOut,
-} from "../animations/transitions";
 import { getDimensionsForLayout } from "../layout/dimensions";
-import { applyBRollRules } from "../scenes/BRoll/apply-b-roll-rules";
+import { addDurationsToScenes } from "./add-durations-to-scenes";
 import { addMetadataToScene } from "./add-metadata-to-scene";
 import { getAllCameras } from "./get-camera";
-
-const PLACEHOLDER_DURATION_IN_FRAMES = 60;
 
 export const calcMetadata: CalculateMetadataFunction<MainProps> = async ({
   props,
@@ -35,103 +27,13 @@ export const calcMetadata: CalculateMetadataFunction<MainProps> = async ({
     ),
   );
 
-  let addedUpDurations = 0;
-  let currentChapter: string | null = null;
-
-  const scenesAndMetadataWithDuration: SceneAndMetadata[] =
-    scenesAndMetadataWithoutDuration.map((sceneAndMetadata, i) => {
-      const previousSceneAndMetaData =
-        scenesAndMetadataWithoutDuration[i - 1] ?? null;
-      const nextSceneAndMetaData =
-        scenesAndMetadataWithoutDuration[i + 1] ?? null;
-
-      const isTransitioningIn = previousSceneAndMetaData
-        ? getShouldTransitionIn({
-            previousScene: previousSceneAndMetaData,
-            scene: sceneAndMetadata,
-            canvasLayout: props.canvasLayout,
-          })
-        : false;
-
-      const isTransitioningOut = getShouldTransitionOut({
-        sceneAndMetadata,
-        nextScene: nextSceneAndMetaData,
-        canvasLayout: props.canvasLayout,
-      });
-
-      if (isTransitioningIn) {
-        addedUpDurations -= SCENE_TRANSITION_DURATION;
-      }
-
-      const from = addedUpDurations;
-      addedUpDurations += sceneAndMetadata.durationInFrames;
-
-      if (sceneAndMetadata.type === "other-scene") {
-        return {
-          ...sceneAndMetadata,
-          from,
-        };
-      }
-
-      let adjustedDuration = sceneAndMetadata.durationInFrames;
-
-      let transitionAdjustedStartFrame = sceneAndMetadata.startFrame;
-
-      if (isTransitioningIn) {
-        transitionAdjustedStartFrame = Math.max(
-          0,
-          sceneAndMetadata.startFrame - SCENE_TRANSITION_DURATION,
-        );
-
-        const additionalTransitionFrames =
-          sceneAndMetadata.startFrame - transitionAdjustedStartFrame;
-
-        addedUpDurations += additionalTransitionFrames;
-        adjustedDuration += additionalTransitionFrames;
-      }
-
-      if (sceneAndMetadata.scene.newChapter) {
-        currentChapter = sceneAndMetadata.scene.newChapter;
-      }
-
-      const retValue: SceneAndMetadata = {
-        ...sceneAndMetadata,
-        bRolls: applyBRollRules({
-          bRolls: sceneAndMetadata.bRolls,
-          sceneDurationInFrames: adjustedDuration,
-          willTransitionToNextScene: isTransitioningOut,
-        }),
-        startFrame: transitionAdjustedStartFrame,
-        durationInFrames: adjustedDuration,
-        from,
-        chapter: currentChapter,
-      };
-
-      if (sceneAndMetadata.scene.stopChapteringAfterThis) {
-        currentChapter = null;
-      }
-
-      return retValue;
-    });
-
-  if (scenesAndMetadataWithDuration.length === 0) {
-    scenesAndMetadataWithDuration.push({
-      type: "other-scene" as const,
-      scene: {
-        type: "noscenes" as const,
-        music: "none",
-        transitionToNextScene: true,
-      },
-      durationInFrames: PLACEHOLDER_DURATION_IN_FRAMES,
-      from: 0,
-    });
-    addedUpDurations += PLACEHOLDER_DURATION_IN_FRAMES;
-  }
+  const { durationInFrames, scenesAndMetadataWithDuration } =
+    addDurationsToScenes(scenesAndMetadataWithoutDuration, props.canvasLayout);
 
   const { height, width } = getDimensionsForLayout(props.canvasLayout);
 
   return {
-    durationInFrames: addedUpDurations,
+    durationInFrames,
     height,
     width,
     fps: FPS,
