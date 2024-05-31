85e205b2907c69f77b1afa59ed7eb9ae9986e1f9
Jonny Burger <jonathanburger11@gmail.com>
2024-05-31 16:58:40 +0200

    Update handle-video.ts

--- scripts/server/handle-video.ts
+++ scripts/server/handle-video.ts
@@ -1,5 +1,6 @@
 import type { Request, Response } from "express";
 import fs, { createWriteStream } from "fs";
+import os from "os";
 import path from "path";
 import { convertAndRemoveSilence } from "../convert-video";
 
@@ -19,28 +20,28 @@ export const handleVideoUpload = async (req: Request, res: Response) => {
       throw new Error("No `folder` provided");
     }
 
-    const file = `${prefix}${endDateAsString}.webm`;
+    const file = `${prefix}${endDateAsString}.mp4`;
 
     const folderPath = path.join(process.cwd(), "public", folder);
     const filePath = path.join(folderPath, file);
+    const input = path.join(os.tmpdir(), Math.random() + ".webm");
 
+    fs.mkdirSync(path.dirname(input), { recursive: true });
     fs.mkdirSync(path.dirname(filePath), { recursive: true });
 
-    const writeStream = createWriteStream(filePath);
+    const writeStream = createWriteStream(input);
 
     req.pipe(writeStream);
 
     await new Promise((resolve) => writeStream.on("finish", resolve));
 
     convertAndRemoveSilence({
-      input: filePath,
-      output: filePath.replace(".webm", ".mp4"),
+      input: input,
+      output: filePath,
     });
 
-    req.on("end", () => {
-      res.statusCode = 200;
-      res.end(JSON.stringify({ success: true }));
-    });
+    res.statusCode = 200;
+    res.end(JSON.stringify({ success: true }));
   } catch (e) {
     console.error(e);
     res.statusCode = 500;
