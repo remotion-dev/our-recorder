0b63c7e1fa1363a22454d8414649ffab0b130da8
Jonny Burger <jonathanburger11@gmail.com>
2024-07-29 16:29:51 +0200

    Update DisplayBlur.tsx

--- remotion/scenes/VideoScene/DisplayBlur.tsx
+++ remotion/scenes/VideoScene/DisplayBlur.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { AbsoluteFill, OffthreadVideo } from "remotion";
 import { Rect } from "../../layout/layout-types";
 
@@ -8,25 +8,28 @@ export const DisplayBlur: React.FC<{
   src: string;
   dimensions: Rect;
 }> = ({ src, dimensions }) => {
+  const containerStyle: React.CSSProperties = useMemo(() => {
+    return {
+      background: "black",
+    };
+  }, []);
+
+  const style: React.CSSProperties = useMemo(() => {
+    return {
+      width: dimensions.width + OVERSCAN * 2,
+      height: dimensions.height + OVERSCAN * 2,
+      top: dimensions.top - OVERSCAN,
+      left: dimensions.left - OVERSCAN,
+      position: "absolute",
+      objectFit: "cover",
+      filter: `blur(${OVERSCAN}px)`,
+      opacity: 0.5,
+    };
+  }, [dimensions]);
+
   return (
-    <AbsoluteFill
-      style={{
-        background: "black",
-      }}
-    >
-      <OffthreadVideo
-        style={{
-          width: dimensions.width + OVERSCAN * 2,
-          height: dimensions.height + OVERSCAN * 2,
-          top: dimensions.top - OVERSCAN,
-          left: dimensions.left - OVERSCAN,
-          position: "absolute",
-          objectFit: "cover",
-          filter: `blur(${OVERSCAN}px)`,
-          opacity: 0.5,
-        }}
-        src={src}
-      />
+    <AbsoluteFill style={containerStyle}>
+      <OffthreadVideo style={style} src={src} />
     </AbsoluteFill>
   );
 };
